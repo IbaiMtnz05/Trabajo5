d4678db1fb9415361415d84de2bf44caebb6e5b8
Ibai Martinez <77854404+IbaiMtnz05@users.noreply.github.com>
2025-02-21 16:13:51 +0000

    new.c carga hasta data

--- new.c
+++ new.c
@@ -5,14 +5,15 @@
 #include <stdlib.h> // atoi
 #include <string.h> // strtok
 #include <sys/wait.h>
+#include <errno.h> //control de errores
 #define stacksize 1048576
 
 static double **data;
-int data_nrows;
+int data_nrows;// = 60000; // CAUTION CUIDADO REVISAR
 int data_ncols = 784;
-char *my_path = ""; // tendréis que poner vuestro path
+char *my_path = "/workspaces/Trabajo5/"; // tendréis que poner vuestro path
 
-int seed = 3; // user acaf---3
+int seed = 3;
 int matrices_rows[4] = {784, 200, 100, 50};
 int matrices_columns[4] = {200, 100, 50, 10};
 int vector_rows[4] = {200, 100, 50, 10};
@@ -41,12 +42,13 @@ int read_matrix(double **mat, char *file, int nrows, int ncols, int fac) {
      * matriz (de dimensión nrows x ncols) de datos contenida en el fichero con
      * nombre file
      */
-
-    char *buffer; // Esto contendrá toda la fila
+    printf("\nRead matrix\n");
+    char *buffer = malloc(data_nrows * sizeof(double)); // Esto contendrá toda la fila
     char *record;  //Esto contendrá las columnas de la fila
     FILE *fstream = fopen(file, "r");
     double aux;
     // Hay que hacer control de errores
+    control_errores(file);
     for (int row = 0; row < nrows; row++) {
 	// Leer, separar, y reservar columnas de la fila
         for (int column = 0; column < ncols; column++) {
@@ -61,6 +63,7 @@ int read_matrix(double **mat, char *file, int nrows, int ncols, int fac) {
         }
     }
     // Hay que cerrar ficheros y liberar memoria
+    printf("cerrando archivos y liberando memoria\n");
     fclose(fstream);
     free(buffer);
     free(record);
@@ -73,24 +76,26 @@ int read_vector(double *vect, char *file, int nrows) {
      * (nrows), deja en vect el vector (de dimensión nrows) de datos contenido en
      * el fichero con nombre file
      */
-    char *buffer; // Esto contendrá el valor
+    printf("Read vector\n");
+    char *buffer = malloc(2048*sizeof(double)); // Esto contendrá el valor
     FILE *fstream = fopen(file, "r");
     double aux;
     // Control de errores
+    control_errores(file);
     for (int row = 0; row < nrows; row++) {
         // leer el valor
         aux = strtod(buffer, NULL);
         vect[row] = aux;
     }
     // Hay que cerrar ficheros y liberar memoria
+    printf("Cerrando archivos y liberando memoria \n");
     fclose(fstream);
     free(buffer);
     return 0;
 }
 
 
-void print_matrix(double **mat, int nrows, int ncols, int offset_row,
-                  int offset_col) {
+void print_matrix(double **mat, int nrows, int ncols, int offset_row, int offset_col) {
     /*
      * Dada una matriz (mat), una cantidad de filas (nrows) y columnas (ncols) a
      * imprimir, y una cantidad de filas (offset_row) y columnas (offset_col) a
@@ -111,22 +116,58 @@ void load_data(char *path) {
      * Dado un directorio en el que están los datos y parámetros, los carga en las
      * variables de entorno
      */
-
-    digits = malloc(data_nrows *
-                    sizeof(double)); // Los valores que idealmente predeciremos
+    str = malloc(128);
+    digits = malloc(data_nrows * sizeof(double)); // Los valores que idealmente predeciremos
     sprintf(str, "%scsvs/digits.csv", path);
     read_vector(digits, str, data_nrows);
+    printf("digits cargados\n");
+
+    data = malloc(data_ncols * data_nrows * sizeof(double));
+    sprintf(str, "%scsvs/data.csv", path);
+    read_matrix(data, str, data_nrows, data_ncols, 1);
+    printf("data cargada\n");
 
     // Las matrices
     mat1 = malloc(matrices_rows[0] * sizeof(*mat1));
-    sprintf(str, "%sparameters/weights%d_%d.csv", path, 3, seed);
+    sprintf(str, "%sparameters/weights%d_%d.csv", path, 0, seed);
     read_matrix(mat1, str, matrices_rows[0], matrices_columns[0], 1);
+    printf("mat1 cargada\n");
+
+    mat2 = malloc(matrices_rows[0] * sizeof(*mat2));
+    sprintf(str, "%sparameters/weights%d_%d.csv", path, 1, seed);
+    read_matrix(mat1, str, matrices_rows[1], matrices_columns[1], 1);
+    printf("mat2 cargada\n");
+
+    mat3 = malloc(matrices_rows[0] * sizeof(*mat3));
+    sprintf(str, "%sparameters/weights%d_%d.csv", path, 2, seed);
+    read_matrix(mat1, str, matrices_rows[2], matrices_columns[2], 1);
+    printf("mat3 cargada\n");
+    
+    mat4 = malloc(matrices_rows[0] * sizeof(*mat4));
+    sprintf(str, "%sparameters/weights%d_%d.csv", path, 3, seed);
+    read_matrix(mat1, str, matrices_rows[3], matrices_columns[3], 1);
+    printf("mat4 cargada\n");
 
     // Los vectores
     vec1 = malloc(vector_rows[0] * sizeof(double));
-    sprintf(str, "%sparameters/biases%d_%d.csv", path, 3, seed);
+    sprintf(str, "%sparameters/biases%d_%d.csv", path, 0, seed);
     read_vector(vec1, str, vector_rows[0]);
+    printf("vec1 cargada\n");
 
+    vec2 = malloc(vector_rows[0] * sizeof(double));
+    sprintf(str, "%sparameters/biases%d_%d.csv", path, 1, seed);
+    read_vector(vec1, str, vector_rows[1]);
+    printf("vec2 cargada\n");
+
+    vec3 = malloc(vector_rows[0] * sizeof(double));
+    sprintf(str, "%sparameters/biases%d_%d.csv", path, 2, seed);
+    read_vector(vec1, str, vector_rows[2]);
+    printf("vec3 cargada\n");
+
+    vec4 = malloc(vector_rows[0] * sizeof(double));
+    sprintf(str, "%sparameters/biases%d_%d.csv", path, 3, seed);
+    read_vector(vec1, str, vector_rows[3]);
+    printf("vec4 cargada\n");
 }
 
 void unload_data() {
@@ -167,4 +208,19 @@ int main(int argc, char *argv[]) {
     
     unload_data();
     return 0;
+}
+
+int control_errores(const char *checkFile) {
+    FILE *f = fopen(checkFile, "r");
+    
+    if (f == NULL) {
+        printf("errno: %d\n", errno);
+        printf("Error: %s\n", strerror(errno));
+        perror("Houston, tenemos un problema");
+        return 1; // Indica error
+    }
+    
+    printf("No tenemos problemas con el archivo: %s\n", checkFile);
+    fclose(f); // Cerrar el archivo si se abrió correctamente
+    return 0; // Indica que no hubo error
 }
